fb71fa7fed20f0d31fe9a5e56ef52e59950a310e
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-04-22 22:17:52 -0700

    Update Whats-a-name-in.js

--- Whats-a-name-in.js
+++ Whats-a-name-in.js
@@ -40,3 +40,17 @@ Examples:
 
 
 // Solution
+
+function nameInStr(str, name){
+  name = name.toLowerCase()
+  str = str.toLowerCase()
+  
+  let index = 0
+  
+  for (let char of str) {
+    if (char === name[index])
+      index++
+  }
+  
+  return index === name.length
+}
